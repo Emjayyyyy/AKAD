e737e973f8b4581ac508b0723b24bda155ed9ed3
Mohammed Jamal <82827087+Emjayyyyy@users.noreply.github.com>
2022-01-21 14:32:13 +0530

    Create #1  Square(n) Sum.py
    21st Jan 2021

---
+++ #1  Square(n) Sum.py
@@ -0,0 +1,16 @@
+#-----DETAILS-----
+
+
+#Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+#For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
+
+
+
+#-----SOLUTION------
+
+def square_sum(numbers):
+    return sum([i**2 for i in numbers])
+
+numbers = [2,5,10,1]
+print(square_sum(numbers))   
